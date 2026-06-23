ef20e53fba297f311bff6f12758fd2f13f455e12
yy232372 <ymlee22@uos.ac.kr>
2026-06-23 17:12:09 +0900

    wego_ez colcon build

--- src/wego_ez/setup.py
+++ src/wego_ez/setup.py
@@ -9,8 +9,11 @@ setup(
     packages=find_packages(exclude=['test']),
     package_dir={'': '.'},
     data_files=[
+        ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), ['launch/wego_ez_launch.py']),
+        (os.path.join('share', package_name, 'launch'),
+        ['launch/wego_ez_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
