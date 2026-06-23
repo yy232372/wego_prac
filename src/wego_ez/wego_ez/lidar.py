761e150189795520584b964852e3dc3ffe04ea7f
yy232372 <ymlee22@uos.ac.kr>
2026-06-23 17:33:51 +0900

    print distance

--- src/wego_ez/wego_ez/lidar.py
+++ src/wego_ez/wego_ez/lidar.py
@@ -1,38 +1,5 @@
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import Bool 
-from sensor_msgs.msg import LaserScan
-from math import pi, cos, sin, sqrt
-
-class DistanceCalculator(Node):
-    def __init__(self):
-        super().__init__('distance_calculator')
-        self.subscription = self.create_subscription(
-            LaserScan,
-            'scan',
-            self.laser_callback,
-            rclpy.qos.qos_profile_sensor_data)
-        self.subscription  # prevent unused variable warning
-
-        self.publisher_ = self.create_publisher(float, 'front_dist', 10)
-        
-    def laser_callback(self, msg):
-        front_dist = float('inf')
-
-        for i, data in enumerate(msg.ranges):
-            current_angle = msg.angle_min + msg.angle_increment*i
-            cx = data * cos(current_angle)
-            cy = data * sin(current_angle)
-            if cx > 0.01:
-                dist = round(sqrt(cx**2+cy**2), 2)
-                front_dist.data = min(front_dist, dist)
-
-        self.get_logger().info(f"The closest distance: {front_dist:.2f} m")
-        self.publisher_.publish(front_dist)
-
-import rclpy
-from rclpy.node import Node
 from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 from math import cos, sin, sqrt, isfinite
@@ -64,6 +31,8 @@ class DistanceCalculator(Node):
 
         if front_dist == float('inf'):
             front_dist = 0.0
+
+        self.get_logger().info(f"The closest distance: {front_dist:.2f} m")
 
         cal_dist = Float32()
         cal_dist.data = round(front_dist, 2)
