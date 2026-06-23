b9fb92d9b3cfef98528ab95398e4f9e75aef10ee
yy232372 <ymlee22@uos.ac.kr>
2026-06-23 22:51:48 +0900

    colcon build ez2

--- src/wego_ez2/launch/wego_ez_launch.py
+++ src/wego_ez2/launch/wego_ez_launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     return LaunchDescription([
         # 1. 원본 /scan을 받아서 ROI 적용 후 /scan_roi 발행
         Node(
-            package='wego_ez',
+            package='wego_ez2',
             executable='scan_roi_filter',
             name='scan_roi_filter',
             output='screen'
@@ -14,7 +14,7 @@ def generate_launch_description():
 
         # 2. ROI 처리된 /scan_roi를 받아서 전방 최단거리 계산
         Node(
-            package='wego_ez',
+            package='wego_ez2',
             executable='distance_calculator',
             name='distance_calculator',
             output='screen'
@@ -22,7 +22,7 @@ def generate_launch_description():
 
         # 3. 거리값 /front_dist를 받아서 STOP / SLOW / DRIVE 판단
         Node(
-            package='wego_ez',
+            package='wego_ez2',
             executable='safety_decision',
             name='safety_decision',
             output='screen'
@@ -30,7 +30,7 @@ def generate_launch_description():
 
         # 4. 안전 상태 /safety_state를 받아서 /cmd_vel 발행
         Node(
-            package='wego_ez',
+            package='wego_ez2',
             executable='drive_limo',
             name='drive_limo',
             output='screen'
